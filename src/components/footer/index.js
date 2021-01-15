5eea40ccba78471c3a45ecee0718d9061846d331
saida179 <69127945+saida179@users.noreply.github.com>
2021-01-14 19:01:17 -0700

    working on about me page

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -11,9 +11,9 @@ function Footer() {
                     <Col className="social-media" size="md-12">
                         <a href="https://www.linkedin.com/in/zaida-arevalo-123a071b3/" rel="noreferrer" target="_blank"><i
                             class="fab fa-linkedin-in social-icon"></i></a>
-                        <a href="https://github.com/alligatormonday" rel="noreferrer" target="_blank"><i class="fab fa-github social-icon"></i></a>
+                        <a href="https://github.com/saida179" rel="noreferrer" target="_blank"><i class="fab fa-github social-icon"></i></a>
                     </Col>
-                    <span style={{ margin: '0 auto', alignContent: 'center', textAlign: 'center' }}>Handcrafted by Joseph Jepson &#169; 2021 All Rights Reserved</span>
+                    <span style={{ margin: '0 auto', alignContent: 'center', textAlign: 'center' }}>Saida Arevalo &#169; 2021 All Rights Reserved</span>
                 </Row>
             </footer>
         </Container>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,6 @@
-import Hero from "../components/Hero/index";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import {FaTerminal} from "react-icons/fa"
-
-
 function About() {
     return (
-        
+
     );
 }
 
